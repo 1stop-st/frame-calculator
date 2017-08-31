a66bddad89ccbfa28fa5b460265147b5e8e7ee7d
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-08-31 09:07:51 +0000

    Add setup files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='frame-calculator',
+    version='0.1',
+    description='Structural calculator for frame structure.',
+    author='1stop-st.org',
+    author_email='ikeda_hiroki@icloud.com',
+    url='https://github.com/1stop-st/frame-calculator',
+    packages=['frame_calculator']
+)
